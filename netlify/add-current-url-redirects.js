150de648607f4ceec24119c9a647679d4e1fc4f1
Dan Allen <dan@opendevise.com>
2020-05-07 04:09:14 -0600

    upgrade Antora to 2.3 and rename playbook file (PR #183)

---
+++ netlify/add-current-url-redirects.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+NGINX_REWRITES_FILE = process.argv[2] || '../etc/nginx/snippets/rewrites.conf'
+NETLIFY_REDIRECTS_FILE = 'public/_redirects'
+
+const rewrites = fs.readFileSync(NGINX_REWRITES_FILE, 'utf8')
+const redirects = fs.readFileSync(NETLIFY_REDIRECTS_FILE, 'utf8').trimRight()
+const currentUrlRedirects = [...rewrites.matchAll(/^set \$current_version_(?!\S+_api )(\S+) '([^']+)';/gm)].map(([, name, version]) => {
+  name = name.replace(/_/g, '-').replace(/^(connector|sdk)-(.+)/, '$2-$1')
+  return `/${name}/current/* /${name}/${version}/:splat 302!`
+}).join('\n')
+fs.writeFileSync(NETLIFY_REDIRECTS_FILE, currentUrlRedirects + '\n' + redirects + '\n', 'utf8')
