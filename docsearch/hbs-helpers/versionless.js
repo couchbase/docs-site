df329bdbd72ac193ade211a5bca00397f20170c3
Dan Allen <dan@opendevise.com>
2021-02-04 16:07:37 -0700

    revert additional of tutorials component to search index [skip ci]
    reverts commit 155f3bb5464a79c9b68acad43f557756cfb4f55c

--- docsearch/hbs-helpers/versionless.js
+++ docsearch/hbs-helpers/versionless.js
@@ -1,3 +1,3 @@
-const versionless = (components) => components.filter(({ name, latest }) => name !== 'home' && latest === 'master')
+const versionless = (components) => components.filter(({ name, latest }) => name !== 'home' && name !== 'tutorials' && latest === 'master')
 
 module.exports.register = (hbs) => hbs.registerHelper('versionless', versionless)
