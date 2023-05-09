a7e482df779e8efed86cd22e9829e67399541187
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-05-09 14:26:22 +0100

    Fix each-openapi templating
    * Now linked and ready for POC in Mobile
    (TODO remaining for additionalProperties, and will need iteration)
    
    * markdown-block now uses a library (as the markdown in the new
    OpenAPI spec benefits from this)

--- home/modules/contribute/examples/helpers/each-openapi.js
+++ home/modules/contribute/examples/helpers/each-openapi.js
@@ -1,9 +1,17 @@
-// iterate openapi property definitions
+// Format config section of openapi property definitions.
+// We pass in a data structure from an openapi Schema.
+//
+//     Table of Contents (of the tree of this Schema, in HTML, linked to below)
+//
+//     The Asciidoc template is called for each node of the tree
+//
+// see extensions-template.adoc for motivating example.
 
 
-module.exports = function(context, options) {    
-    const indent = "   "
+module.exports = function(context, options) {
     
+    // setup some helper variables
+    const indent = "   " 
     const types = 
     {
         integer: { fallback: '0', format: parseInt },
@@ -35,14 +43,22 @@ module.exports = function(context, options) {
     }
     
     // recursive function to walk the Table of Contents
+    // NOTE: we are building up HTML here, because Asciidoc is hard to generate for something that's 
+    // richly marked up and whitespace dependent.
     function process_toc (node, path) {
         
-        const path_id = path.join('.')
-        const left = path.length ? 
-            `<a href="#${path_id}">${path[path.length-1]}</a>: ` 
-            : ''
-        const level = indent.repeat(path.length)
+        // the Left of the JSON expression (except at the Root) is `key: `
+        // IF the related node has a Description, then link to it.
         
+        const left = path.length ?
+            node.description ?
+                `<a href="#${path.join('-')}">${path[path.length-1]}</a>: `
+                : `${path[path.length-1]}: `
+            : ''
+
+        const level = indent.repeat(path.length)
+
+        // recursively descend into child properties
         if (node.type == 'object') {
             if (node.properties) {
                 const children = 
@@ -60,19 +76,54 @@ module.exports = function(context, options) {
                 return `${level}(TODO additionalProperties)`
             }
         }
+        // If we're a leaf node, then just show the Right hand side (example or type)
         else {
             const right = format_type(node)
             return `${level}${left}${right}`
         }
     }
+
+    // recursive function to flatten the tree into a flat list like [root, a, a.a1, a.a1.a2, a.b1, a.b1.b2, ...]
+    function get_items ({node, path}) {
+        if (node.type == 'object') {
+            if (node.properties) {
+                const children = 
+                    Object.entries(node.properties)
+                        .sort()
+                        .map(
+                            ([child_key,child]) => get_items(
+                                {node: child, path: [...path, child_key]}))
+                        
+                return [{node, path}, ...children.flat()]
+            }
+        }
+        else {
+            return [{node, path}]
+        }
+    }
     
+        
+    // Generate Table of Contents HTML, and wrap the whole thing in a passthrough block (++++)
     const table_of_contents = [
         '++++',
         '<pre>',
         process_toc(context, []),
         '</pre>',
+        '++++'
     ].join('\n')
+    
+    // Now generate the body
+    const body_text =
+            get_items({node: context, path: []})
+            .slice(1) // remove the wrapper Root node
+            .map(item => options.fn( // call the Handlebars template with our data
+                item,
+                // pass @path_id and @path_id_href to Handlebars template
+                { data:    {
+                    path_id: item?.path?.join('.'),
+                    path_id_href: item?.path?.join('-')
+                }}))
+            .join('\n\n')
 
-    console.log(table_of_contents)
-    return new global.handlebars.SafeString(table_of_contents)
+    return new global.handlebars.SafeString(table_of_contents + '\n\n' + body_text)
 }
