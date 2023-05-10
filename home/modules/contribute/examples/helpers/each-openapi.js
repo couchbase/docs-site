274013c37e6e57ddcb5bb1d16769eeb46302a561
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-05-10 16:07:12 +0100

    refactor

--- home/modules/contribute/examples/helpers/each-openapi.js
+++ home/modules/contribute/examples/helpers/each-openapi.js
@@ -45,65 +45,63 @@ module.exports = function(context, options) {
     // recursive function to walk the Table of Contents
     // NOTE: we are building up HTML here, because Asciidoc is hard to generate for something that's 
     // richly marked up and whitespace dependent.
-    function process_toc (node, path) {
+    function process_toc (node) {
         
         // the Left of the JSON expression (except at the Root) is `key: `
         // IF the related node has a Description, then link to it.
         
-        const left = path.length ?
-            node.description ?
-                `<a href="#${path.join('-')}">${path[path.length-1]}</a>: `
-                : `${path[path.length-1]}: `
-            : ''
+        const left = (node, path) =>
+            path.length ?
+                node.description ?
+                    `<a href="#${path.join('-')}">${path[path.length-1]}</a>: `
+                    : `${path[path.length-1]}: `
+                : ''
 
-        const level = indent.repeat(path.length)
+        const level = (path) => indent.repeat(path.length)
+        
+        const internal = (node, path, children) => 
+            level(path) + left(node, path) + '{\n' +
+            children.join(',\n') + '\n' +
+            level(path) + '}'
 
-        // recursively descend into child properties
-        if (node.type == 'object') {
-            if (node.properties) {
-                const children = 
-                    Object.entries(node.properties)
-                        .filter(([_,child]) => ! child.deprecated)
-                        .sort()
-                        .map(
-                            ([child_key,child]) => process_toc(child, [...path, child_key]))
-                        
-                return `${level}${left}{\n` +
-                        children.join(',\n') +
-                    `\n${level}}`               
-            }
-            else {
-                // TODO handle additionalProperties
-                return `${level}(TODO additionalProperties)`
-            }
-        }
-        // If we're a leaf node, then just show the Right hand side (example or type)
-        else {
-            const right = format_type(node)
-            return `${level}${left}${right}`
-        }
+        const leaf = (node, path) =>
+            level(path) + left(node, path) + format_type(node)
+    
+        return iterate_properties(node, leaf, internal)
     }
 
     // recursive function to flatten the tree into a flat list like [root, a, a.a1, a.a1.a2, a.b1, a.b1.b2, ...]
     // TODO refactor (the iteration is identical to above, but this version *flattens* the result into a list)
     // Looks like we just need to parametrize `leaf` and `internal` functions.
-    function get_items ({node, path}) {
-        if (node.type == 'object') {
-            if (node.properties) {
-                const children = 
-                    Object.entries(node.properties)
-                    .filter(([_,child]) => ! child.deprecated)
-                    .sort()
-                        .map(
-                            ([child_key,child]) => get_items(
-                                {node: child, path: [...path, child_key]}))
-                        
-                return [{node, path}, ...children.flat()]
+    function iterate_properties (node, leaf, internal) {
+        
+        function recurse(node, path) {
+            if (node.type == 'object') {
+                if (node.properties) {
+                    const children = 
+                        Object.entries(node.properties)
+                        .filter(([_,child]) => ! child.deprecated)
+                        .sort()
+                            .map(
+                                ([child_key,child]) => recurse(child, [...path, child_key]))
+                    return internal(node, path, children)
+                } else {
+                    // something something additionalProperties
+                }
+            }
+            else {
+                return leaf(node, path)
             }
         }
-        else {
-            return [{node, path}]
-        }
+        
+        return recurse(node, [])
+    }
+    
+    function get_items(node) {
+        const leaf = (node, path) => [{node, path}]
+        const internal = (node, path, children) => [{node, path}, ...children.flat()]
+        
+        return iterate_properties(node, leaf, internal)
     }
     
         
@@ -111,14 +109,14 @@ module.exports = function(context, options) {
     const table_of_contents = [
         '++++',
         '<pre>',
-        process_toc(context, []),
+        process_toc(context),
         '</pre>',
         '++++'
     ].join('\n')
     
     // Now generate the body
     const body_text =
-            get_items({node: context, path: []})
+            get_items(context)
             .slice(1) // remove the wrapper Root node
             .map(item => options.fn( // call the Handlebars template with our data
                 item,
