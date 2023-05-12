79d4a07ace2bcca5463a28d05d561250c6ee1a33
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-05-12 09:01:43 +0100

    Refactor and fix each-openapi

--- home/modules/contribute/examples/helpers/each-openapi.js
+++ home/modules/contribute/examples/helpers/each-openapi.js
@@ -6,9 +6,10 @@
 //     The Asciidoc template is called for each node of the tree
 //
 // see extensions-template.adoc for motivating example.
+'use strict'
 
-
-module.exports = function(context, options) {
+module.exports = function(context, ...schemas) {
+    const options = schemas.pop()
     
     // setup some helper variables
     const indent = "   " 
@@ -37,10 +38,12 @@ module.exports = function(context, options) {
             types[node.type]?.fallback ??
             node.type ??
             '(unknown)'
-    
+        
         const format = types[node.type]?.format ?? (val => val)
         return format(type) 
     }
+    
+    const path_id_href = (path) => path.map(v=>v.replace(/\./g, '')).join('-')
     
     // recursive function to walk the Table of Contents
     // NOTE: we are building up HTML here, because Asciidoc is hard to generate for something that's 
@@ -53,7 +56,7 @@ module.exports = function(context, options) {
         const left = (node, path) =>
             path.length ?
                 node.description ?
-                    `<a href="#${path.join('-')}">${path[path.length-1]}</a>: `
+                    `<a href="#${path_id_href(path)}">${path[path.length-1]}</a>: `
                     : `${path[path.length-1]}: `
                 : ''
 
@@ -69,6 +72,31 @@ module.exports = function(context, options) {
     
         return iterate_properties(node, leaf, internal)
     }
+    
+    // The usual path to get to child objects is `properties` which includes specific, *named* properties.
+    //
+    // `additionalProperties` are unrestrained collections.
+    // They can also be specified as a boolean, which is a shorthand for a default object.
+    
+    function child_entries(node) {
+        const via_properties = 
+            node.properties ? 
+                Object.entries(node.properties) : []
+        
+        function format_additionalProperties (ap) {
+            if (typeof(ap) == 'boolean') ap = {}
+            return { ...{type: "object"}, ...ap }                                       
+        }
+        
+        const via_additionalProperties =
+            node.additionalProperties ?
+                [[
+                    `{${node.additionalProperties["x-additionalPropertiesName"] || "additionalProperties"}...}`,
+                    format_additionalProperties(node.additionalProperties) ]]
+                : []
+        
+        return [...via_properties, ...via_additionalProperties]
+    }
 
     // recursive function to flatten the tree into a flat list like [root, a, a.a1, a.a1.a2, a.b1, a.b1.b2, ...]
     // TODO refactor (the iteration is identical to above, but this version *flattens* the result into a list)
@@ -77,17 +105,14 @@ module.exports = function(context, options) {
         
         function recurse(node, path) {
             if (node.type == 'object') {
-                if (node.properties) {
-                    const children = 
-                        Object.entries(node.properties)
-                        .filter(([_,child]) => ! child.deprecated)
-                        .sort()
-                            .map(
-                                ([child_key,child]) => recurse(child, [...path, child_key]))
-                    return internal(node, path, children)
-                } else {
-                    // something something additionalProperties
-                }
+
+                const children = 
+                    child_entries(node)
+                    .filter(([_,child]) => ! child.deprecated)
+                    .sort()
+                        .map(
+                            ([child_key,child]) => recurse(child, [...path, child_key]))
+                return internal(node, path, children)
             }
             else {
                 return leaf(node, path)
@@ -104,6 +129,14 @@ module.exports = function(context, options) {
         return iterate_properties(node, leaf, internal)
     }
     
+    // workaround for displaying e.g. Role + User schemas
+    if (schemas.length) {
+        const root = { properties: {}, type: "object" }
+        for (var schema of schemas) {
+            root.properties[schema] = context[schema]
+        } 
+        context = root
+    }    
         
     // Generate Table of Contents HTML, and wrap the whole thing in a passthrough block (++++)
     const table_of_contents = [
@@ -113,7 +146,7 @@ module.exports = function(context, options) {
         '</pre>',
         '++++'
     ].join('\n')
-    
+        
     // Now generate the body
     const body_text =
             get_items(context)
@@ -123,7 +156,7 @@ module.exports = function(context, options) {
                 // pass @path_id and @path_id_href to Handlebars template
                 { data:    {
                     path_id: item?.path?.join('.'),
-                    path_id_href: item?.path?.join('-')
+                    path_id_href: path_id_href(item?.path || [])
                 }}))
             .join('\n\n')
 
