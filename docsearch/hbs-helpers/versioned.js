f1e880abcf1d76422b14087c7c62152346cc14d4
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-05-24 15:54:26 +0100

    Update helpers to get better results for capella

--- docsearch/hbs-helpers/versioned.js
+++ docsearch/hbs-helpers/versioned.js
@@ -1,3 +1,3 @@
-const versioned = (components) => components.filter(({ latest }) => latest !== 'master')
+const versioned = (components) => components.filter(({ latest }) => (latest || 'master') != 'master')
 
 module.exports.register = (hbs) => hbs.registerHelper('versioned', versioned)
--- docsearch/hbs-helpers/versionless.js
+++ docsearch/hbs-helpers/versionless.js
@@ -1,3 +1,3 @@
-const versionless = (components) => components.filter(({ name, latest }) => name !== 'home' && name !== 'tutorials' && latest === 'master')
+const versionless = (components) => components.filter(({ name, latest }) => name !== 'home' && name !== 'tutorials' && (latest || 'master') === 'master')
 
 module.exports.register = (hbs) => hbs.registerHelper('versionless', versionless)
