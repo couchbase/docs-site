43bb7f01e0e44f4749e7fbbc244f732bb84e8fea
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-05-26 15:04:42 +0100

    refactor helpers for readability

--- docsearch/hbs-helpers/versionless.js
+++ docsearch/hbs-helpers/versionless.js
@@ -1,3 +1,17 @@
-const versionless = (components) => components.filter(({ name, latest }) => name !== 'home' && name !== 'tutorials' && (latest || 'master') === 'master')
+// A component with an empty version (defined as `~` in the playbook.yml) 
+// or named 'master' is considered to be "versionless", so the URL generated
+// will be simply e.g. https://docs.couchbase.com/cloud/
+// whereas a "versioned" component might have /current/ or /7.1/ appended.
+//
+// Note that 'home' and 'tutorials' are special-cased elsewhere in the template,
+// and therefore excluded from this filter.
+
+const versionless = (components) => components.filter(
+    ({ name, latest }) =>
+        name !== 'home' 
+        && name !== 'tutorials'
+        && (
+            latest === 'master'
+            || latest === ''))
 
 module.exports.register = (hbs) => hbs.registerHelper('versionless', versionless)
