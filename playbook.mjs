122523133d0e102ece3da6ded3eb68faff94b74f
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-07-17 11:23:10 +0100

    Refactor, add $seq, remove git calls
    Simplified the logic by inverting it and creating the immutable tree
    piece by piece, instead of mutating the existing playbook.
    
    Added $seq to run multiple functions in pipeline.
    
    Removed the git calls, as the HEAD branch handling didn't really work,
    and required a bunch of messy async/await.

--- playbook.mjs
+++ playbook.mjs
@@ -29,40 +29,47 @@ $.verbose = false
  * $patch: contains the tree to change
  * $meta: replaced with metadata
  *
- * $add: add an item to the end of a list
+ ** $add: add an item to the end of a list
  * $prepend: add an item to the beginning of list
  * e.g:
+
+      $patch:
         asciidoc:
           extensions:
             $add: 
               - '@asciidoctor/tabs'
 
- * $without: remove an item from a list
+ ** $without: remove an item from a list
  * e.g:
+
+      $patch:
         asciidoc:
           extensions:
             $without: asciidoctor-external-callout
 
 
- * $filter: restrict a list of objects, where the given fields match
+ ** $filter: restrict a list of objects, where the given fields match
  * e.g:
  * In this example, only those sources which have `tags: mobile` will be selected.
  * You can provide multiple key/values that must all match.
  *
+      $patch:
         content:
           sources:
             $filter:
               tags: mobile
 
- * $prune: Keep only X branches
+ ** $prune: Keep only X branches
  * This operator is specialised for content.sources
  * e.g.
  * Here we only keep the first 2 branches (for example ([release/7.2, release/7.1]`)
+
+      $patch:
         content:
           sources:
             $prune: 2
 
- * $local: resolve sources to the ones you have checked out locally
+ ** $local: resolve sources to the ones you have checked out locally
  * e.g.
  * We work through all of the sources like `https://github.com/couchbasecloud/couchbase-cloud`
  * and look in each of the `paths` provided to see if there is a local directory matching.
@@ -80,6 +87,7 @@ $.verbose = false
  * If the `only` parameter is true, then we will remove all the remote sources.
  * If `head` is supplied, then use the HEAD branch (e.g. the version you have checked out)
 
+      $patch:
         content:
           sources:
             $local:
@@ -87,19 +95,22 @@ $.verbose = false
               only: true
               head:
       
- * $only: as an alternative to $filter, restrict the sources to specific repos
+ ** $only: as an alternative to $filter, restrict the sources to specific repos
  * e.g:
  * (note how you can use the short name of the source)
+
+      $patch:
         content:
           sources:
             $only:
               - docs-sync-gateway
               - docs-couchbase-lite
       
- * $override: update a specific source (using the short name) to apply any other operators.
+ ** $override: update a specific source (using the short name) to apply any other operators.
  * e.g.:
  * Add `my-test-branch` as the *first* branch of `docs-sync-gateway`
  *
+      $patch:
         content:
           sources:
             $override:
@@ -108,8 +119,18 @@ $.verbose = false
                   $prepend:
                     - my-test-branch
  * 
+ ** $seq: apply multiple operators in turn to the same node
+ *
+    $patch:
+      content:
+          sources:
+            $seq:
+              - $prune: 1
+              - $local:
+                  paths:
+                    - ..
+ *
  * See patch.yml and patch-staging.yml for full examples.
- * Notice how these operators can be used with each other.
  *
  */
 const patchFile = argv.patch ?? argv._.shift() ?? 'patch.yml'
@@ -119,8 +140,6 @@ const $patch = patch.$patch
 if (! $patch) {
   throw new Error("Patch file is missing $patch tree")
 }
-
-
 
 /** Helper function to wrap a single item in an array if required */
 const toArray = (thing) => Array.isArray(thing) ? thing : [thing]
@@ -154,17 +173,16 @@ function cmp (a, b) {
     }
 }
 
-
 /**
   * @param {Array.<>} node - any array node in playbook
   * @param {Object} filters - object of key/values to filter for (using `cmp` function)
   *                           All provided filters must match.
   */
-function $filter  (orig, param) {
-    if (! Array.isArray(orig)) {
+function $filter  (node, param) {
+    if (! Array.isArray(node)) {
         throw new Error(`$filter run on something that isn't an array! ${JSON.stringify(param)}`)
     }
-    return orig.filter(item =>
+    return node.filter(item =>
         Object.entries(param)
             .every(([k,v]) => cmp(item[k], v)))
 }
@@ -176,64 +194,39 @@ function $filter  (orig, param) {
   * @param {boolean}        config.only - if true, filter out all non-local sources
   * @param {boolean}        config.head - if true, only use HEAD
   *
-  * We try to play nice with Author-mode by substituting HEAD if it matches a 
-  * branch, or prepending it otherwise.
-  * BUT this doesn't work great with feature-branches (really, we should check the antora.yml version)
-  * In the meantime, use config.head as workaround
   */
-async function $local (orig, {paths, only, head}) {
+ function $local (sources, {paths, only, head}) {
     const repoPaths = toArray(paths ?? ['..'])
-    const withLocal = await Promise.all(
-        orig.map(async item => {
-            const {url, branches} = item
+    
+    const overrideHead = head ? {branches: ['HEAD']} : {}
+    
+    return sources.flatMap(item => {
+            const {url} = item
             
+            // if url is already a local path, then wave it through
             if (url.match(/^[.\/]/)) {
-                return {...item, local: true}
+                return [{...item, local: true}]
             }
 
+            // check all of the possible paths
             const repo = path.basename(url, '.git')
             for (const repoPath of repoPaths) {
                 const newPath = path.join(repoPath, repo)
                 try {
                     fs.accessSync(newPath, fs.constants.R_OK)
                     
-                    if (head) {
-                        return {...item, url: newPath, branches: ['HEAD'], local: true}
-                    }
-                    else {
-                        // cleverness to try to match branches to HEAD
-                        const HEAD = (await $`git -C ${newPath} rev-parse HEAD`).stdout.trim()
-                        
-                        const b1 = await Promise.all(branches.map(async b => {
-                            try {
-                                const ref = (await $`git -C ${newPath} rev-parse ${b}`).stdout.trim()
-                                return (ref === HEAD) ? 'HEAD' : b
-                            }
-                            catch (e) {
-                                // filter this one out as presumably it doesn't exist locally
-                                return 
-                            }
-                        }))
-                        const b2 = b1.filter(a=>a !== undefined)
-                        const b3 = b2.includes('HEAD') ? b2 : ['HEAD', ...b2]
-                        return {...item, url: newPath, branches: b3, local: true}
-                    }
+                    return [{...item, url: newPath, local: true, ...overrideHead}]
                 }
                 catch(e) {
                     continue
                     // do nothing as file doesn't exist here
                 }
             }
-            return item
-        })
-    )
             
-    if (only) {
-        return withLocal.filter(({local}) => local)
-    }
-    else {
-        return withLocal
-    }
+            // Otherwise we either filter out, or return the value unchanged
+            return only ? [] : [item]
+        }
+    )
 }
 
 /**
@@ -247,52 +240,49 @@ function $prune (sources, limit) {
             : source)
 }
 
-const matchRepo = (url, source) => {
-    if (url === '.') {
-        return source === 'docs-site'
-    }
-    return path.basename(url, '.git') === source
-}
+const repoShort = (url) => url === '.' ?  'docs-site' : path.basename(url, '.git')
 
 /**
   * @param {Object[]} sources - expects to be called only on `content.sources` from playbook
   * @param {Object} overrides - name/Object pairs to override
   * @param {Object} overrides."some-repo" - the patch description to apply to the matching source with.
   */
-async function $override (sources, overrides) {
-    for (const [source, override] of Object.entries(overrides)) {
-        const idx = sources.findIndex(({url}) => matchRepo(url, source))
-        if (idx >= 0) {
-            await apply_patch(sources[idx], override)
-        }
-        else {
-            throw new Error(`Source ${source} not found to $override`)
-        }
+function $override (sources, overrides) {
+  return sources.map(source => {
+    const short = repoShort(source.url)
+    if (short in overrides) {
+      return apply_patch(source, overrides[short])
     }
-
-    return sources
+    else {
+      return source
+    }
+  })
 }
 
 /**
   * @param {Object[]} sources - expects to be called only on `content.sources` from playbook
   * @param {Array.<string>} only - just the names of repos to keep
   */
-async function $only (sources, only) {   
+function $only (sources, only) {   
     return sources.filter(({url, tags}) => 
-        tags === 'ALL' || only.some(source => matchRepo(url, source)))
+        tags === 'ALL' || only.some(source => source === repoShort(url)))
+}
+
+function $seq (node, seq) {
+  return seq.reduce(apply_function, node)
 }
 
 
 // Catalog of functions
 const functions = {
     // explicit replace
-    $replace: (_orig, param) => _.cloneDeep(param), // avoid YAML aliasing
+    $replace: (node, param) => _.cloneDeep(param), // avoid YAML aliasing
     
     // append or prepend the data to an ARRAY
-    $append:  (orig, param) => [...orig, ...param],
-    $prepend: (orig, param) => [...param, ...orig],
+    $append:  (node, param) => [...node, ...param],
+    $prepend: (node, param) => [...node, ...param],
     // remove item from array
-    $without: (orig, param) => orig.filter(item => ! param.includes(item)),
+    $without: (node, param) => node.filter(item => ! param.includes(item)),
 
     // filter a list by matching key
     $filter,
@@ -301,37 +291,55 @@ const functions = {
     $prune,
     $local,
     $override,
-    $only
+    $only,
+    
+    // Sequence functions
+    $seq
+}
+
+
+const is_function = (obj) => {
+  if (is_object(obj)) {
+    const keys = Object.keys(obj)
+    return (keys.length === 1) && keys[0] in functions
+  }
+}
+
+const is_object = (node) => (node && typeof node === 'object' && ! Array.isArray(node))
+
+const apply_function = (node, patch) => {
+  const [fnName, param] = Object.entries(patch)[0]
+  const fn = functions[fnName]
+  return fn(node, param)
 }
 
 /**
  * Recursive function to apply the patch, by descending both the original
  * playbook and the patch at the same time, applying operators as required.
  */
-async function apply_patch(node, patch) {
+function apply_patch(node, patch) {
+  
+  // while `null` is a valid value for a  node, or a patch replacement,
+  // `undefined` signals the absence of a value.
+  // Therefore we return the other side.
+  if (patch === undefined) return node
 
-    for (const [k,v] of Object.entries(patch)) {
+  // Apply functions
+  if (is_function(patch)) {
+    return apply_function(node, patch)
+  }
 
-        // If the patch file contains an object, then... 
-        if (v !== null && typeof v === 'object' && ! Array.isArray(v)) {
-            for (const [kk, vv] of Object.entries(v)) {
-                // EITHER: apply the $operator, if it's found in the functions catalog
-                if (kk in functions) {
-                    const ret = await functions[kk](node[k], vv)
-                    node[k] = ret
-                }
-                // OR descend both sides of the tree and recurse
-                else {
-                    await apply_patch(node[k], patch[k])
-                }
-            }
-        }
-        // Otherwise (e.g. a plain value or an array) then we just assume this is a replacement value.
-        else {
-            node[k] = _.cloneDeep(v) // avoid YAML aliasing
-        }
-    }
+  // if both sides are objects, we iterate the superset of keys
+  if (is_object(node) && is_object(patch)) {
+    const keys = Object.keys({...node, ...patch})
+    return Object.fromEntries(keys.map(k => [k, apply_patch(node[k], patch[k])]))
+  }
+  
+  // the only remaining option: return the patched value.
+  // we clone it, to avoid aliasing from the `$patch` section
+  return _.cloneDeep(patch)
 }
+
 
 var update = argv.update
 const playbookFile = patch.$from ?? argv.playbook ?? argv._.shift() ?? 'antora-playbook.yml'
@@ -342,22 +350,22 @@ if (argv.truncate) {
   playbook = { $from: playbookFile, $patch }
 }
 else {
-  playbook = YAML.parse(fs.readFileSync(playbookFile).toString())
+  const original = YAML.parse(fs.readFileSync(playbookFile).toString())
 
   // Confirm which files we are composing
   console.error(`Composing ${playbookFile} with ${patchFile}`)
 
-
-  await apply_patch(playbook, $patch)
+  const patched = apply_patch(original, $patch)
 
   const $meta = {
     date: Date()
   }
 
-  playbook = { $from: playbookFile, $meta, $patch, ...playbook }
+  playbook = { $from: playbookFile, $meta, $patch, ...patched }
 }
 
 const output = YAML.stringify(playbook)
+
 if (update) {
   try {
     fs.copySync(patchFile, `${patchFile}~`)
