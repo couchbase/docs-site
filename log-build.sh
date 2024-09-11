11147157e4fec9bec21ee62091e8fb7649b1d235
Ray Offiah <ray.offiah@couchbase.com>
2024-09-11 10:42:05 +0100

    [DOC-12476]: Making sure the log-build.sh is not tied to your machine.

--- log-build.sh
+++ log-build.sh
@@ -8,11 +8,9 @@ FONTAWESOME_NPM_AUTH_TOKEN=0A6E1AEF-9C2E-40DC-B01E-4C7E702F83B3 npm install
 
 cd .. || exit
 
-echo "Creating folder (if it doesn't exist) ..."
-mkdir /Users/rayoffiah/projects/couchbase/rayoffiah.github.io/"$1";
+echo "Running log build ..."
 
 echo "Building site ..."
-npx antora --clean --stacktrace ./doc-12476-antora-playbook.yml --to-dir /Users/rayoffiah/projects/couchbase/rayoffiah.github.io/"$1";
+npx antora --clean --stacktrace ./doc-12476-antora-playbook.yml
 
-echo "Populating FontAwesome icon set ..."
-node ./scripts/populate-icon-defs.js /Users/rayoffiah/projects/couchbase/rayoffiah.github.io/"$1";
+
