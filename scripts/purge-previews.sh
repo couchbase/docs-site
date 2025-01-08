94a52f21cfa4e204ba0d027fbbab0350c4890ea7
Hakim Cassimally <hakim.cassimally@couchbase.com>
2025-01-08 14:02:36 +0000

    date fiddling for cross-platform

--- scripts/purge-previews.sh
+++ scripts/purge-previews.sh
@@ -4,7 +4,24 @@ INPUT=builds.json
 
 PURGE_DAYS=${PURGE_DAYS-30}
 
-SINCE=`date -v -${PURGE_DAYS}d -I`
+# `date` is different on Mac/Linux so we have to do some fiddling
+UNAME="$(uname -s)"
+case "${UNAME}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    MSYS_NT*)   machine=MSys;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine == "Mac" ];
+then
+    SINCE=`date -v -${PURGE_DAYS}d -I`
+else
+    SINCE=`date -d "{PURGE_DAYS} days ago" -I`
+fi
+
 
 jq  --arg SINCE "$SINCE" \
     -r \
