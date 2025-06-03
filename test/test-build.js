808e8c88e43898d6705a055bfb547f24f27b8503
Hakim Cassimally <hakim.cassimally@couchbase.com>
2025-06-03 17:07:37 +0100

    fix tests

--- test/test-build.js
+++ test/test-build.js
@@ -8,7 +8,7 @@ const cheerio = require('cheerio')
 describe('antora build with extensions', function () {
 
   before('run the antora build', function () {
-    this.timeout(10000) // inrease timeout for the Antora build
+    this.timeout(10000) // increase timeout for the Antora build
     spawnSync('antora', [
       '--log-level', 'fatal', 
       '--to-dir', 'test/build',
@@ -16,34 +16,34 @@ describe('antora build with extensions', function () {
     {'stdio': 'inherit'})
   })
 
-  const files = 
-    fs.readdirSync('test/fixtures/antora-output/').filter(
-      file => file.match(/^extension.*\.html$/))
-  
-    for (const file of files) {
-      const expected = fs.readFileSync(`test/fixtures/antora-output/${file}`, 'utf8')
+  ok('test files', function () { 
+    // wanted these to happen after the build, so looks like they need to be within an OK?
+    const files = 
+      fs.readdirSync('test/fixtures/antora-output/').filter(
+        file => file.match(/^extension.*\.html$/))
+    
+      for (const file of files) {
+        const expected = fs.readFileSync(`test/fixtures/antora-output/${file}`, 'utf8')
 
-      let actualHtml
-      try {
-        const actual = fs.readFileSync(`test/build/home/contribute/${file}`, 'utf8')
+        let actualHtml
+        try {
+          const actual = fs.readFileSync(`test/build/home/contribute/${file}`, 'utf8')
 
-        const $ = cheerio.load(actual)
-        actualHtml = $('article').html()
-      }
-      catch (err) {
-        // We expect some files to NOT exist (e.g. to test embargoed content)
-        // so we just return an empty string.
-        // This means we can test the embargoed content by supplying a
-        // zero-length fixture.
+          const $ = cheerio.load(actual)
+          actualHtml = $('article').html()
+        }
+        catch (err) {
+          // We expect some files to NOT exist (e.g. to test embargoed content)
+          // so we just return an empty string.
+          // This means we can test the embargoed content by supplying a
+          // zero-length fixture.
 
-        // console.error(`Error reading actual file for ${file}:`, err)
-        actualHtml = ''
-      }
-      fs.writeFileSync(`test/fixtures/antora-output/${file}.actual`, actualHtml)
+          console.error(`Error reading actual file for ${file}:`, err)
+          actualHtml = ''
+        }
+        fs.writeFileSync(`test/fixtures/antora-output/${file}.actual`, actualHtml)
 
-      ok(file, function () { 
         assert.equal(actualHtml, expected)
-      })
     }
-
+  })
 })
