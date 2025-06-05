d1ae670a259c263a39af7c6c2c19d1851d6af473
Hakim <hakim.cassimally@couchbase.com>
2025-06-05 09:17:26 +0100

    DOC-13194, DOC-12510: improve openapi/JSON Schema handling (#823)
    * DOC-13194, DOC-12510: improve openapi/JSON Schema handling
    
    * infer type: object if appropriate
    Though it would be convenient for us if all nodes were tagged,
    redocly doesn't declare that as an issue with the spec, so
    we should handle it.
    
    * descend into `allOf`
    
    * move each-openapi.js into helpers
    
    * DOC-13194 DOC-12510: test each-openapi
    
    Run `npm test` from the root of docs-site to test
    
    * DOC-13194 additional tests via @torcolvin
    
    * tidying
    
    * DOC-13194 simple output test for extensions content.
    
    * DOC-13194 simplify
    
    * DOC-13194 npm-test github action
    
    basic github action
    
    * DOC-13194 add playbook for testing
    
    * debug
    
    * fix tests
    
    * fix tests better
    
    I'd not understood the Mocha logic of when code executes...
    e.g.
    the `ok` blocks are run at the equivalent of "compile time".
    They then register the *intent* to run the test *function*.
    
    So we need to do the file read *inside* the registered function.
    
    * indentation

---
+++ test/test-build.js
@@ -0,0 +1,48 @@
+// this test file is designed to be run with Mocha
+const assert = require('assert')
+const fs = require('fs')
+const ok = specify
+const { spawnSync } = require('node:child_process')
+const cheerio = require('cheerio')
+
+describe('antora build with extensions', function () {
+
+  before('run the antora build', function () {
+    this.timeout(10000) // increase timeout for the Antora build
+    spawnSync('antora', [
+      '--log-level', 'fatal', 
+      '--to-dir', 'test/build',
+      'antora-playbook-test.yml'],
+      {'stdio': 'inherit'})
+  })
+
+  const files = 
+  fs.readdirSync('test/fixtures/antora-output/').filter(
+    file => file.match(/^extension.*\.html$/))
+
+  for (const file of files) {
+
+    ok(`test file ${file}`, function () { 
+        const expected = fs.readFileSync(`test/fixtures/antora-output/${file}`, 'utf8')
+
+        let actualHtml
+        try {
+          const actual = fs.readFileSync(`test/build/home/contribute/${file}`, 'utf8')
+
+          const $ = cheerio.load(actual)
+          actualHtml = $('article').html()
+        }
+        catch (err) {
+          // We expect some files to NOT exist (e.g. to test embargoed content)
+          // so we just return an empty string.
+          // This means we can test the embargoed content by supplying a
+          // zero-length fixture.
+          // console.error(`Error reading actual file for ${file}:`, err)
+          actualHtml = ''
+        }
+        fs.writeFileSync(`test/fixtures/antora-output/${file}.actual`, actualHtml)
+
+        assert.equal(actualHtml, expected)
+      })
+    }
+  })
