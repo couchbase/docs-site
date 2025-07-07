77208eb3436478cce90974242e6c68d3d2a0628e
Hakim Cassimally <hakim.cassimally@couchbase.com>
2025-07-07 16:58:10 +0100

    WIP WIP

--- scripts/make-site-digest.sh
+++ scripts/make-site-digest.sh
@@ -3,11 +3,17 @@
 MANIFEST=${1-public/site-manifest.json}
 DIGEST=${2-public/site-digest.txt}
 
+# TODO rewrite this in Javascript?
+# update the MD5 into the digest file, instead of a .txt file.
+# then any output can be handled with a template iteration.
+# (This may be more similar to what OpenDevise end up doing in any case.)
+
 for URL in $(
     jq -r \
         '.components | .. | select(.url?) | select(.alias? | not) |  .url' \
         $MANIFEST \
-    | grep -v '^null$')
+    | grep -v '^null$' \
+    | sort -u)
 do
     FILE=public$URL
     # fillet out just the <main> section of the HTML file
